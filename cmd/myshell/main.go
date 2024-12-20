32e4346047f4ab204dc8e5d3ec7312480be5d20c
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2024-12-20 15:03:00 +0000

    Implemented type command to check if a command is builtin or not

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,6 +8,24 @@ import (
 	"strings"
 )
 
+// function that checks if a mentioned command is built-in or not
+func isBuiltIn(value string) bool {
+	commands := []string{
+		"exit",
+		"echo",
+		"type",
+	}
+
+	// iterating through the list of known built-in commands
+	for i := range commands {
+		if commands[i] == value {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
@@ -25,31 +43,31 @@ func main() {
 
 		switch command[0] {
 		case "exit":
-			code, err := strconv.Atoi(command[1])
-
-			if err != nil {
-				fmt.Fprintf(os.Stdout, "%s: command not found\n", message)
-				os.Exit(1)
+			if len(command) > 1 {
+				code, err := strconv.Atoi(command[1])
+	
+				if err != nil {
+					fmt.Fprintf(os.Stdout, "%s: command not found\n", message)
+					os.Exit(1)
+				}
+				os.Exit(code)
+			} else {
+				fmt.Fprintf(os.Stdout, "Mention exit code\n")
 			}
-			os.Exit(code)
 		
 		case "echo":
 			fmt.Fprintf(os.Stdout, "%s\n", strings.Join(command[1:], " "))
 		
+		case "type":
+			value := strings.Join(command[1:], " ")
+			if isBuiltIn(value) {
+				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", value)
+			} else {
+				fmt.Fprintf(os.Stdout, "%s: not found\n", value)
+			}
 		default:
 			fmt.Fprintf(os.Stdout, "%s: command not found\n", message)
 		}
-
-		// exit 0 -> command to exit the shell
-		// if command == "exit 0\n" {
-		// 	os.Exit(0)
-		// }
-
-		// if strings.HasPrefix(command, "echo") {
-		// 	fmt.Println(strings.TrimLeft(command[4:len(command)-1], " "))
-		// } else {
-		// 	fmt.Println(command[:len(command)-1] + ": command not found")
-		// }
 
 	}
 }
