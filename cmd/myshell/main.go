5e89d95f355034d1342711aa37fe9890e403c0fd
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2024-12-20 14:39:50 +0000

    Implemented echo command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,30 +4,52 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	for i:=0; ;i++ {
+	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		
 		// Wait for user input
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		message, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input: ", err)
 			os.Exit(1)
 		}
 
-		// exit 0 -> command to exit the shell
-		if command == "exit 0\n" {
-			os.Exit(0)
+		message = strings.TrimSpace(message)
+
+		command := strings.Split(message, " ")
+
+		switch command[0] {
+		case "exit":
+			code, err := strconv.Atoi(command[1])
+
+			if err != nil {
+				fmt.Fprintf(os.Stdout, "%s: command not found\n", message)
+				os.Exit(1)
+			}
+			os.Exit(code)
+		
+		case "echo":
+			fmt.Fprintf(os.Stdout, "%s\n", strings.Join(command[1:], " "))
+		
+		default:
+			fmt.Fprintf(os.Stdout, "%s: command not found\n", message)
 		}
 
-		if strings.HasPrefix(command, "echo") {
-			fmt.Println(strings.TrimLeft(command[4:len(command)-1], " "))
-		} else {
-			fmt.Println(command[:len(command)-1] + ": command not found")
-		}
+		// exit 0 -> command to exit the shell
+		// if command == "exit 0\n" {
+		// 	os.Exit(0)
+		// }
+
+		// if strings.HasPrefix(command, "echo") {
+		// 	fmt.Println(strings.TrimLeft(command[4:len(command)-1], " "))
+		// } else {
+		// 	fmt.Println(command[:len(command)-1] + ": command not found")
+		// }
 
 	}
 }
